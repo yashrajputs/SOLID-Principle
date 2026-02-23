137f6e5bf5c9609647c43601814d38b9e9578863
Yash Rajput <122536776+yashrajputs@users.noreply.github.com>
2026-02-23 13:23:23 +0530

    Add DiscountService class and Discount interface
    Implemented DiscountService class to calculate discounts based on type. Added Discount interface for future extensibility.

---
+++ O--->Open, Closed Principle example(Discount).java
@@ -0,0 +1,20 @@
+public class DiscountService{
+  double calculateDiscount(String type){
+    if(type.equals("seasonal")){
+      return 10.0;
+    }else if(type.equals("clearance")){
+      return 20.0;
+    }
+    return 0.0;
+  }
+}
+
+//Using Polymorphism
+//Discount will be the main partent type
+// Create new class
+//No modification of old logic
+public interface Discount{
+   
+
+}
+  
